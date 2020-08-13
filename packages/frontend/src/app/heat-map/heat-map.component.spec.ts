1a40986ee6bd50a52c11719dfd54268cfba5bea1
Ismael Gbian <gismael@google.com>
2020-08-13 19:06:52 -0400

    fix: lint

--- packages/frontend/src/app/heat-map/heat-map.component.spec.ts
+++ packages/frontend/src/app/heat-map/heat-map.component.spec.ts
@@ -12,7 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {async, ComponentFixture, TestBed, fakeAsync, tick} from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 import {HeatMapComponent} from './heat-map.component';
 import {of, empty} from 'rxjs';
 import {COMService} from '../services/com/com.service';
