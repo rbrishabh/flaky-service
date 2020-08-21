a15db5bc3799c96ac8ba02ddaf40de9b4869a9cb
Cedonia Peterson <cedoniap@google.com>
2020-08-21 13:30:06 -0400

    jsdoc

--- packages/api/lib/xunit-parser.js
+++ packages/api/lib/xunit-parser.js
@@ -17,10 +17,11 @@ const TestCaseRun = require('../lib/testrun');
 
 class Parser {
   /**
-  Parse the xunit test results, converting them to an array of testrun objects.
-  Each of these testrun objects contains whether the test passed, its name, and
-  its failure message (if failed).
-  @param xmlString - The complete xunit string of test results sent to the endpoint
+  * Parse the xunit test results, converting them to an array of testrun objects.
+  * Each of these testrun objects contains whether the test passed, its name, and
+  * its failure message (if failed).
+  * @param xmlString {string} The complete xunit string of test results sent to the endpoint
+  * @returns {string[]} An array of testrun objects representing all test runs
   */
   parse (xmlString) {
     const obj = xmljs.xml2js(xmlString, { compact: true });
@@ -86,9 +87,9 @@ class Parser {
   }
 
   /**
-  The url contains 'github.com/:org/:repo', optionally followed by a path.
-  This method trims the url down to only the string following `repo`, which may be empty.
-  @param url - the Github url of where a test came from
+  * The url contains 'github.com/:org/:repo', optionally followed by a path.
+  * @param url {string} the Github url of where a test came from
+  * @returns {string} the string following `:repo`, which may be empty
   */
   trim (url) {
     const updated = url.replace(/(.)*github.com\/[a-zA-Z-]*\/[a-zA-Z-]*/g, '');
@@ -98,8 +99,9 @@ class Parser {
 
   // IMPORTANT: All values that will be used as keys in Firestore must be escaped with the firestoreEncode function
   /**
-  Parse the build data and convert it into a JSON format that can easily be read and stored.
-  @param metadata - contains all data sent to the endpoint besides the actual test results.
+  * Parse the build data and convert it into a JSON format that can easily be read and stored.
+  * @param metadata Contains all data sent to the endpoint besides the actual test results.
+  * @returns {JSON} The metadata for the build
   */
   cleanXunitBuildInfo (metadata) {
     return {};
